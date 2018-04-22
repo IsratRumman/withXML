9b62b580da2cf8a8d5151bc9356d9fa388ec6dcf
BUP <isratrumman25@gmail.com>
2018-04-23 02:50:40 +0600

    with xml

---
+++ src/com/pageservlet2/Servlet.java
@@ -0,0 +1,19 @@
+package com.pageservlet2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("ami ghash khai");
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: BUP
+  Date: 4/23/2018
+  Time: 2:32 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <a href="/secB">click</a>
+  $END$
+  </body>
+</html>
